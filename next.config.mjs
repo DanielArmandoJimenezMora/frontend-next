253f9fcc90fce9e6b2a59017553c5951d17625e2
Daniel Jimenez Mora <danieljimenezmora@gmail.com>
2022-08-03 19:50:11 -0500

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
-/** @type {import('next').NextConfig} */
-import withPWA from "next-pwa";
+/* @type {import('next').NextConfig} */
+/* import withPWA from "next-pwa";
 
 export default withPWA({
   pwa: {
@@ -8,3 +8,4 @@ export default withPWA({
     skipWaiting: true,
   },
 });
+ */
